85557fa93ba16f26a2f81b9b28be3c9f5dab2e13
eadarak00 <eadarak2001@gmail.com>
2025-08-09 01:05:30 +0000

    feat: Créer l’adaptateur NoteAdapter

---
+++ app/src/main/java/com/example/noteplus/adapter/NoteAdapter.java
@@ -0,0 +1,49 @@
+package com.example.noteplus.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.noteplus.R;
+import com.example.noteplus.model.Note;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private List<Note> notes;
+
+    public NoteAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_note, parent, false);
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.textTitle.setText(note.getTitle());
+        holder.textDate.setText(note.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public static class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView textTitle, textDate;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDate = itemView.findViewById(R.id.textDate);
+        }
+    }
+}
